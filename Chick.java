7e2da2586af4915b476ba8d69d58444e5f543f70
anbao <andychasiubao@gmail.com>
2017-11-08 14:32:29 -0800

    Added first name to index.html

--- Chick.java
+++ Chick.java
@@ -1,4 +1,29 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+     private String myType;
+     private String mySound;
+     private String otherSound;
+     public Chick(String type, String sound)
+     {
+       myType = "chick";
+       mySound = "cluck";
+       otherSound = "cheet";
+     }
+     public String getSound()
+     {
+      
+      if(Math.random() > .5)
+      {
+        return mySound;
+      }
+     else
+      {
+       return otherSound;
+      }
+     }
+     public String getType()
+     {
+       return myType;
+       
+     }
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,11 @@
 
 void setup()
 {
-	//your variable declarations here
+  Cow deku = new Cow("cow", "moo");
+  Chick todo = new Chick("chick", "cluck");
+  NamedCow baku = new NamedCow("cow", "moo", "All Might");
+  System.out.println(deku.getType() + " goes " + deku.getSound());
+  System.out.println(todo.getType() + " goes " + todo.getSound());
+  
+	
 }
-
